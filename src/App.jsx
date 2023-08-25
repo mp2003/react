f65fbee78c11ab048c098f1e2f6235d1514355fc
mahi5256 <m.p6092003@gmail.com>
2023-08-25 19:44:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <button>+</button>
+      <input type="number" readOnly/>
+      <button>-</button>
+    </>
+  );
+};
+
+export default App;
