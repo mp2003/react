936aebda97ffcfae49961e6a66516dab18fd408d
mahi5256 <m.p6092003@gmail.com>
2023-08-25 20:07:29 +0530

    counter ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  let [setValue, setSetValue] = useState(0);
+  let setCounter = (value) => {
+    if (value === "+") return setSetValue(setValue+1);
+    else if (value === "-") return setSetValue(setValue-1);
+    else if (value === "reset") return setSetValue(0)
+  };
   return (
     <>
-      <button>+</button>
-      <input type="number" readOnly/>
-      <button>-</button>
+      <button
+        onClick={() => {
+          setCounter("+");
+        }}
+      >
+        +
+      </button>
+      <input type="text" value={setValue} readOnly />
+      <button
+        onClick={() => {
+          setCounter("-");
+        }}
+      >
+        -
+      </button>
+        <br />
+      <button onClick={() => {
+        setCounter('reset')
+      }}>reset</button>
     </>
   );
 };
+
 export default App;
