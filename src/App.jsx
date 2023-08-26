57ba7e8c110b6fa082cc510e36f18f3917e8fe38
mahi5256 <m.p6092003@gmail.com>
2023-08-26 19:52:41 +0530

    creating drop Dwon menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import Counter from './Counter.jsx';
 import Calculator from "./Calculator.jsx";
+import DropDwon from "./DropDwon.jsx";
+
 const App = () => {
   return (
-    // <Counter />
-    // <Calculator />
+    <>
+    <Counter />
+    <DropDwon />
+    </>
   );
 };
+
 export default App;
---
+++ src/Dummy.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dummy = () => {
+  return (
+    <div><h1>Working</h1></div>
+  )
+}
+
+export default Dummy
