048d3d42fcf3d70e2197164daec8d8fdd0ae8ac2
mahi5256 <m.p6092003@gmail.com>
2023-08-26 21:17:49 +0530

    setting Css

--- src/DropDwon.jsx
+++ src/DropDwon.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink, Routes, Route } from "react-router-dom";
-import './DropDown.css';
+import './DropDwon.css';
 import Dummy from "./Dummy";
 const DropDwon = () => {
   return (
