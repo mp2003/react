0c1587a65c64f7651edf1f3353020377037e3647
mahi5256 <m.p6092003@gmail.com>
2023-08-30 16:00:58 +0530

    Login Visual

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
-  return (<>
-    <div className="heading">
+  return (
+    <>
+      <div className="heading">
         <h2>Welcome to Login</h2>
-        <span className=''>Please enter your Login and your password</span>
-        </div>
+        <span className="">Please enter your Username and your Password</span>
+      </div>
+      <div className="login-detail">
+        <form>
+          <label for="email">Username</label>
+          <input
+            type="email"
+            name="username"
+            placeholder="UserName Or Email"
+          ></input>
+          <label for="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="*********"
+          ></input>
+          <button>Login</button>
+        </form>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
