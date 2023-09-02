91de8ffe66250c009c600f48f966a8002f82071d
mahi5256 <m.p6092003@gmail.com>
2023-09-02 22:29:49 +0530

    added icons

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./NavBar.css";
+import { BiHomeAlt2, BiSearchAlt, BiUser } from "react-icons/bi";
+import { FiCodepen } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <>
+      <div className="container"></div>
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
