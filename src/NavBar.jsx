032b3784fe34cfb05670cf44eb7975de08abb097
mahi5256 <m.p6092003@gmail.com>
2023-09-02 23:05:39 +0530

    added icons

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import "./NavBar.css";
-import { BiHomeAlt2, BiSearchAlt, BiUser } from "react-icons/bi";
-import { FiCodepen } from "react-icons/fi";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { BiHomeAlt2, BiSearchAlt, BiUser, BiLogoReact } from "react-icons/bi";
+import { PiHamburger } from "react-icons/pi";
 
 const NavBar = () => {
   return (
     <>
-      <div className="container"></div>
+      <div className="container">
+        <BiLogoReact className="l-icon" />
+
+        <div className="icon-container">
+          <BiHomeAlt2 className="icon" />
+          
+          <BiSearchAlt className="icon" />
+          
+          <BiUser className="icon" />
+          
+          <PiHamburger className="icon" />  
+          </div>
+      </div>
     </>
   );
 };
