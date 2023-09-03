23d8a61133db510ceb4466afac6f25ee61ce910e
mahi5256 <m.p6092003@gmail.com>
2023-09-03 12:04:35 +0530

    functionality

---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto sunt alias exercitationem? Nam ducimus vel quidem ad possimus? Deleniti dolor quis laboriosam exercitationem, fugit quisquam culpa. Sed, vitae sit.</div>
+  )
+}
+
+export default Home
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
 import { BiHomeAlt2, BiSearchAlt, BiUser, BiLogoReact } from "react-icons/bi";
 import { PiHamburger } from "react-icons/pi";
 import { LiaSignOutAltSolid } from "react-icons/lia";
+import Home from "./Home";
 
 const NavBar = () => {
+  const [showHome, setShowHome] = useState(false);
+  const toggleHome =() => {
+    setShowHome(!showHome);
+  }
   return (
     <>
       <div className="container">
         <div className="icon-container">
           <BiLogoReact className="l-icon" />
 
-          <BiHomeAlt2 className="icon" />
+          <BiHomeAlt2
+            className="icon"
+            onClick={toggleHome}
+          />
 
           <BiSearchAlt className="icon" />
 
